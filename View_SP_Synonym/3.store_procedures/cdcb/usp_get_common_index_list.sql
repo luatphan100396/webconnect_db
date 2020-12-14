cd1e187db03bfcfe3029ed2f460ccac8a7fb700d
linh <pthuylinh@tma.com.vn>
2020-12-14 15:59:30 +0700

    WC-343 add usp_Get_Common_Index_List

---
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_index_list.sql
@@ -0,0 +1,31 @@
+ CREATE OR REPLACE PROCEDURE usp_Get_Common_Index_List
+--================================================================================
+--Author: Linh Pham
+--Created Date: 2020-0-13
+--Description: Get index list(defined by user)  
+--Output: 
+--       +Ds1: table with options used for filter animals
+--=================================================================================
+(
+)
+  
+  dynamic result sets 1
+BEGIN
+    
+	-- TRAIT list   
+	BEGIN
+		DECLARE cursor5  CURSOR WITH RETURN for
+		
+		select 
+			TRAIT AS INDEX, 
+			TRAIT_FULL_NAME,  
+			UNIT, 
+			DECIMAL_ADJUST_CODE
+		from
+		table( fn_Get_List_traits())
+		where TYPE IN ('INDEX')
+         order by OrderBy;
+		OPEN cursor5;
+		  
+	END;
+END
