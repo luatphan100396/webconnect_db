0d26c041bfd53b496c4f0118eb2afd5f7f329c9a
linh <pthuylinh@tma.com.vn>
2020-12-14 16:15:43 +0700

    WC-341 add usp_Get_Common_Breed_List

---
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_breed_list.sql
@@ -0,0 +1,30 @@
+ 
+CREATE OR REPLACE PROCEDURE usp_Get_Common_Breed_List
+--=================================================================================================
+--Author: Linh Pham
+--Created Date: 2020-12-14
+--Description: Get breed list
+--Output: 
+--       +Ds1: Breed table with breed code and breed name for following breeds: AY,WW,JE,HO,MS,GU,BS
+--================================================================================================
+()
+  
+  dynamic result sets 1
+BEGIN
+    
+	 -- breed list   
+	BEGIN
+		DECLARE cursor2  CURSOR WITH RETURN for
+	    SELECT DISTINCT 
+	    BREED_CODE, 
+	    BREED_NAME 
+	    
+	    FROM BREED_TABLE
+	    WHERE BREED_CODE IN ('AY','WW','JE','HO','MS','GU','BS')
+	    ORDER BY  BREED_NAME with UR;
+	    
+		OPEN cursor2;
+		  
+	END;
+	 
+END 
