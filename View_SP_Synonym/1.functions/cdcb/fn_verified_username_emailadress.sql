f241818a67eec4f78abcfd39de906ac7b88be481
Ta Hong Nghi <thnghi@tma.com.vn>
2020-12-31 14:41:11 +0700

    WC-451 add function fn_Verified_UserName_EmailAdress

---
+++ View_SP_Synonym/1.functions/cdcb/fn_verified_username_emailadress.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION fn_Verified_UserName_EmailAdress
+--======================================================
+--Author: Nghi Ta
+--Created Date: 2020-12-19
+--Description: Check whether the input animal has cross referencfe
+--======================================================
+(
+	@USER_NAME VARCHAR(128),
+	@EMAIL_ADDRESS VARCHAR(200)
+) 
+RETURNS INTEGER
+
+LANGUAGE SQL
+BEGIN 
+	DECLARE IS_MATCHED SMALLINT DEFAULT 0;
+	
+	SET IS_MATCHED = (SELECT case when COUNT(1)=1 then 1 else 0 end
+					  from USER_ACCOUNT_TABLE uac
+				      inner join USER_INFO_TABLE u
+				           on u.USER_KEY = uac.USER_KEY
+				       where uac.USER_NAME = @USER_NAME
+				            and u.EMAIL_ADDR = @EMAIL_ADDRESS
+								 );
+ 
+	RETURN IS_MATCHED;
+END
