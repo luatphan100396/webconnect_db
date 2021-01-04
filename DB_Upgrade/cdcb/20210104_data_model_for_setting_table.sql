cb7787502b6ccbb002336d5af78ed0eff9af8b21
Ta Hong Nghi <thnghi@tma.com.vn>
2021-01-04 10:47:52 +0700

    WC-444 create setting_table

---
+++ DB_Upgrade/cdcb/20210104_data_model_for_setting_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE SETTING_TABLE
+ (
+ NAME VARCHAR(128) NOT NULL PRIMARY KEY,
+ INT_VALUE INT NULL,
+ STRING_VALUE VARCHAR(10000),
+ DECIMAL_VALUE decimal(8,2)
+ );
+ 
+  
+  
+ 
+ INSERT INTO SETTING_TABLE(NAME,INT_VALUE)      
+ VALUES     ('Maintenance_Mode',0);	
+
+INSERT INTO SETTING_TABLE(NAME,STRING_VALUE)      
+ VALUES     ('Maintenance_Message','The CDCB database is currently locked. Any submission mode will be processed automatically as soon as the database is unlocked');	
+INSERT INTO SETTING_TABLE(NAME,STRING_VALUE)      
+ VALUES     ('Maintenance_Collor','#E79593');	
