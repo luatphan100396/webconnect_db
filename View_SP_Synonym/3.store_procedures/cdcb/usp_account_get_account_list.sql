b838ab978367213d1855609e3050c7d1ca511dcc
linh <pthuylinh@tma.com.vn>
2021-01-05 11:10:28 +0700

    WC-457 change usp_Account_Get_Account_List

--- View_SP_Synonym/3.store_procedures/cdcb/usp_account_get_account_list.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_account_get_account_list.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE usp_Account_Get_Account_List 
+CREATE OR REPLACE PROCEDURE usp_Account_Get_Account_List
 --======================================================
 --Author: Linh Pham
 --Created Date: 2020-12-31
@@ -168,7 +168,7 @@ P1: BEGIN
 		    select distinct ur.USER_KEY
 		    from SESSION.TmpRoles t
 		    inner join ROLE_TABLE r 
-		   		 on t.ROLE = r.ROLE_SHORT_NAME
+		   		 on lower(t.ROLE) = lower(trim(r.ROLE_SHORT_NAME))
 		    inner join USER_ROLE_TABLE ur
 		         on ur.ROLE_KEY = r.ROLE_KEY
 		)ur
@@ -178,7 +178,7 @@ P1: BEGIN
 		    select distinct ur.USER_KEY
 		    from SESSION.TmpNominator t
 		    inner join DATA_SOURCE_TABLE r 
-		   		 on t.NOMINATOR = r.SOURCE_SHORT_NAME
+		   		 on lower(t.NOMINATOR) = lower(trim(r.SOURCE_SHORT_NAME))
 		    inner join USER_AFFILIATION_TABLE ur
 		         on ur.DATA_SOURCE_KEY = r.DATA_SOURCE_KEY
 		)uN
@@ -193,7 +193,7 @@ P1: BEGIN
 					OR uInfoTable.ORGANIZATION LIKE '%'||v_SEARCH_BY||'%'
 				)
 		)
-		AND (v_GROUP is null or gr.GROUP_SHORT_NAME= v_GROUP)
+		AND (v_GROUP is null or lower(trim(gr.GROUP_SHORT_NAME))= lower(v_GROUP))
 		AND (v_COUNT_ROLE = 0 or ur.USER_KEY is not null)
 		AND (v_COUNT_NOMINATOR = 0 or uN.USER_KEY is not null)
  
@@ -267,10 +267,7 @@ P1: BEGIN
 			ON t.USER_KEY = ur.USER_KEY 
 	LEFT JOIN DATA_SOURCE_TABLE r 
 		 	ON ur.DATA_SOURCE_KEY = r.DATA_SOURCE_KEY
-	WHERE SOURCE_SHORT_NAME='CA'
-	OR SOURCE_SHORT_NAME='WI'
-	OR SOURCE_SHORT_NAME='NC'
-	OR SOURCE_SHORT_NAME='UT'
+	WHERE upper(trim(SOURCE_SHORT_NAME)) in ('CA','WI','NC','UT') 
 		group by t.USER_KEY
 		
 		WITH UR;
@@ -322,5 +319,36 @@ P1: BEGIN
 		    
 	-- Cursor left open for client application
 	OPEN cursor1;
+	END;
+	BEGIN
+	-- Declare cursor
+	DECLARE cursor2 CURSOR WITH RETURN for
+		SELECT 
+			count(1) AS Num_Recs
+		FROM
+		SESSION.TmpListUser t
+		LEFT JOIN SESSION.TmpListRoleUser lRol
+			ON t.USER_KEY= lRol.USER_KEY
+		LEFT JOIN SESSION.TmpListNominatorUser lNom
+			ON t.USER_KEY= lNom.USER_KEY
+		LEFT JOIN SESSION.TmpListLabUser lLab
+			ON t.USER_KEY= lLab.USER_KEY
+		LEFT JOIN SESSION.TmpListDRPCUser lDRPC
+			ON t.USER_KEY= lDRPC.USER_KEY
+		LEFT JOIN USER_INFO_TABLE uInfo
+			ON t.USER_KEY=uInfo.USER_KEY
+		INNER JOIN USER_ACCOUNT_TABLE uAcc
+			ON t.USER_KEY=uAcc.USER_KEY
+		LEFT JOIN USER_GROUP_TABLE uG
+			ON t.USER_KEY=uG.USER_KEY
+		LEFT JOIN GROUP_TABLE gr
+			ON uG.GROUP_KEY= gr.GROUP_KEY
+		LEFT JOIN USER_VISIT_HISTORY_TABLE uvh
+			ON uAcc.USER_KEY=uvh.USER_KEY
+			AND uvh.ID=LAST_LOGIN
+		WITH UR;
+		    
+	-- Cursor left open for client application
+	OPEN cursor2;
 	END;
 END P1
