2a29cdaa05ccc08ba6a29cd53568f23c284b20b1
Đỗ Minh Trí <dminhtri@tma.com.vn>
2021-01-06 14:35:20 +0700

    WC-683 add sp usp_Account_Reject_Account_Request

---
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_account_reject_account_request.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE PROCEDURE usp_Account_Reject_Account_Request
+--======================================================
+--Author: Tri Do
+--Created Date: 2021-01-05
+--Description: Reject Account Request
+--Output: None
+--======================================================
+(
+	@REQUEST_KEY INT
+)
+P1: BEGIN
+	DECLARE V_CURRENT_TIME TIMESTAMP;
+
+	SET v_CURRENT_TIME = (SELECT CURRENT TIMESTAMP FROM SYSIBM.SYSDUMMY1);
+		
+	UPDATE ACCOUNT_REQUEST_TABLE
+	SET STATUS = 'REJECTED'
+		,MODIFIED_TIME = v_CURRENT_TIME
+	WHERE REQUEST_KEY = @REQUEST_KEY;
+END P1
