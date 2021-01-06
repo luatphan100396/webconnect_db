b44569a3896ff6977fa5f835c4380df4a35904e3
linh <pthuylinh@tma.com.vn>
2021-01-06 10:54:13 +0700

    Auto stash before rebase of "CDCB_S04_B02_DB_Linh"

--- View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_role.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_role.sql
@@ -4,7 +4,11 @@ CREATE OR REPLACE PROCEDURE usp_Get_Common_Role
 --Created Date: 2020-01-06
 --Description: Get List role  
 --Output: 
+<<<<<<< HEAD
 --       +Ds1: table with options used for Management account
+=======
+--       +Ds1: table with options used for Management
+>>>>>>> CDCB_S04_B02_DB
 --=================================================================================
 (
 	
@@ -14,8 +18,13 @@ BEGIN
 	DECLARE cursor1 CURSOR WITH RETURN for
 
 	SELECT  
+<<<<<<< HEAD
 		ROLE_SHORT_NAME 
 		,ROLE_NAME
+=======
+		ROLE_NAME,
+		ROLE_SHORT_NAME 
+>>>>>>> CDCB_S04_B02_DB
 	FROM  ROLE_TABLE
 	ORDER BY ROLE_SHORT_NAME
 	WITH UR;
