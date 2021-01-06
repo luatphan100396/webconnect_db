ee1813421b32b82261bc3d642ed45e9dac050b21
linh <pthuylinh@tma.com.vn>
2021-01-06 12:10:28 +0700

    WC-457 5 stores procedure get LAB, DRPC. MININATOR, ROLE, GROUP (3)

--- View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_lab.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_lab.sql
@@ -1,4 +1,4 @@
- CREATE OR REPLACE PROCEDURE usp_Get_Common_Lab
+CREATE OR REPLACE PROCEDURE usp_Get_Common_Lab
  --================================================================================
 --Author: Linh Pham
 --Created Date: 2020-1-6
@@ -13,12 +13,12 @@ BEGIN
 	DECLARE cursor1 CURSOR WITH RETURN for
 
 	SELECT 
-		SOURCE_SHORT_NAME AS LAB_SOURCE_SHORT_NAME
-		,SOURCE_NAME AS LAB_SOURCE_NAME
+		trim(SOURCE_SHORT_NAME) AS LAB_SOURCE_SHORT_NAME
+		,trim(SOURCE_NAME) AS LAB_SOURCE_NAME
 		FROM DB2INST1.DATA_SOURCE_TABLE
 		WHERE CLASS_CODE  = 'L'
 		AND STATUS_CODE  = 'A' 
 		ORDER BY SOURCE_NAME
 		WITH UR;
 		OPEN cursor1;
-END 
+END
