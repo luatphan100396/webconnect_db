c87f57202a291d1aa95b7b5fd60817a8679f6300
Ta Hong Nghi <thnghi@tma.com.vn>
2021-01-07 09:21:01 +0700

    WC-551 fix wrong commit

--- View_SP_Synonym/3.store_procedures/cdcb/usp_login_get_user_affiliations.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_login_get_user_affiliations.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE PROCEDURE usp_login_get_user_access_permission
+CREATE OR REPLACE PROCEDURE usp_login_get_user_affiliations
 --======================================================
 --Author: Nghi Ta
 --Created Date: 2020-12-14
---Description: Get user access permission
+--Description: Get user affiliations
 --Output:
---        +Ds1: Table with features and permissions
+--        +Ds1: Table affiliations
 --======================================================
 (
 	IN @USER_NAME VARCHAR(128)
@@ -15,53 +15,23 @@ BEGIN
    BEGIN
 	DECLARE cursor1 CURSOR WITH RETURN for 
 	
-			  select USER_NAME,
-			 g.GROUP_SHORT_NAME AS GROUP_ROLE,
-			 f.FEATURE_NAME,
-			 fc.COMPONENT_NAME
-		 from 
-		 (
-		 select * 
-		 from USER_ACCOUNT_TABLE
-		  where lower(user_name) = lower(@USER_NAME)
-		 )  
-		 u
-		 inner join USER_GROUP_TABLE ug
-		 	on ug.USER_KEY = u.USER_KEY
-		 inner join GROUP_TABLE g
-		 	on g.GROUP_KEY = ug.GROUP_KEY 
-		 inner join GROUP_FEATURE_COMPONENT_TABLE gfc
-		    on gfc.GROUP_KEY = g.GROUP_KEY 
-		 inner join FEATURE_COMPONENT_TABLE fc
-		    on gfc.COMPONENT_KEY = fc.COMPONENT_KEY
-		 inner join FEATURE_TABLE f
-		    on f.FEATURE_KEY = fc.FEATURE_KEY
-		
-		union
-		 
-		 select USER_NAME,
-			 g.ROLE_SHORT_NAME,
-			 f.FEATURE_NAME,
-			 fc.COMPONENT_NAME
-		 from 
-		 (
-		 select * 
-		 from USER_ACCOUNT_TABLE
-		  where lower(user_name) = lower(@USER_NAME)
-		 )  
-		 u
-		 inner join USER_ROLE_TABLE ug
-		 	on ug.USER_KEY = u.USER_KEY
-		 inner join ROLE_TABLE g
-		 	on g.ROLE_KEY = ug.ROLE_KEY 
-		 inner join ROLE_FEATURE_COMPONENT_TABLE gfc
-		    on gfc.ROLE_KEY = g.ROLE_KEY 
-		 inner join FEATURE_COMPONENT_TABLE fc
-		    on gfc.COMPONENT_KEY = fc.COMPONENT_KEY
-		 inner join FEATURE_TABLE f
-		    on f.FEATURE_KEY = fc.FEATURE_KEY
-		with ur;
-		 
+	 select  d.SOURCE_SHORT_NAME as AFFILIATE_SHORT_NAME,
+	         d.SOURCE_NAME as AFFILIATE_NAME
+	 from 
+	 (
+	 select USER_NAME,
+	        USER_KEY
+	 from USER_ACCOUNT_TABLE
+	  where lower(user_name) = lower(@USER_NAME)
+	 )  
+	 u
+	 inner join USER_AFFILIATION_TABLE uaf
+	 	on uaf.USER_KEY = u.USER_KEY
+	 inner join DATA_SOURCE_TABLE d
+	    on uaf.DATA_SOURCE_KEY = d.DATA_SOURCE_KEY
+	 order by d.SOURCE_SHORT_NAME
+	 with ur;
+	     	  
 	  
 	OPEN cursor1;
 	
