829327c70dec894de8260d6dcd797014f8a29452
linh <pthuylinh@tma.com.vn>
2021-01-12 10:06:31 +0700

    WC-707 add implements stored procedure

---
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_status.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE PROCEDURE usp_Get_Common_Status
+--====================================================================================
+--Author: Linh Pham
+--Created Date: 2020-01-07
+--Description: Get Status use for add group administration
+--Output: 
+--       +Ds1: table with STATUS_CODE
+--=======================================================================================
+()
+	DYNAMIC RESULT SETS 1
+P1: BEGIN
+	-- Declare cursor
+	DECLARE cursor1 CURSOR WITH RETURN for
+	SELECT 
+		DESCRIPTION as STATUS
+	FROM REFERENCE_TABLE
+	WHERE TYPE ='STATUS_CODE'
+	ORDER BY DESCRIPTION
+	with ur;
+
+	-- Cursor left open for client application
+	OPEN cursor1;
+END P1
