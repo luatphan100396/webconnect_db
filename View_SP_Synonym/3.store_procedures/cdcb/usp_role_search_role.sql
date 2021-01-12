c9eba57725fc0f308a94c9628cc1bca653506912
nductuyen <nductuyen@tma.com.vn>
2021-01-12 17:36:14 +0700

    WC-727 commit usp_Role_Search_Role

--- View_SP_Synonym/3.store_procedures/cdcb/usp_role_search_role.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_role_search_role.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE PROCEDURE usp_Role_Search_Role 
 --================================================================================
 --Author: Tuyen Nguyen
---Created Date: 2020-01-05
---Description: Search_Role  
+--Created Date: 2021-01-05
+--Description: Get List Role  
 --Output: 
---       +Ds1: table with List Role
+--       +Ds1: table with list role
 --=================================================================================
 (
   IN @ROLE_NAME VARCHAR(100)
@@ -13,18 +13,27 @@ CREATE OR REPLACE PROCEDURE usp_Role_Search_Role
     
 	DYNAMIC RESULT SETS 1
 P1: BEGIN
-
+    
+	
 	BEGIN
 		DECLARE cursor1 CURSOR WITH RETURN for
 		SELECT 
 		        row_number()over(order by ROLE_NAME) as No,
 		        ROLE_SHORT_NAME,
 				ROLE_NAME,
-				ref.DESCRIPTION as STATUS
+				ref.DESCRIPTION as STATUS,
+				case when ur.ROLE_KEY IS NOT NULL then '0' 
+				     else '1'
+				end as IS_DELETE 
 		FROM ROLE_TABLE r
 		LEFT JOIN REFERENCE_TABLE ref
 			on r.STATUS_CODE = ref.CODE
 			and ref.TYPE ='STATUS_CODE'
+		LEFT JOIN (
+		     select distinct ROLE_KEY
+		     from USER_ROLE_TABLE  
+		)ur
+		    on ur.ROLE_KEY = r.ROLE_KEY
 		WHERE LOWER(ROLE_NAME) LIKE '%'||LOWER(@ROLE_NAME)||'%'
 		ORDER BY ROLE_NAME
 		WITH UR;
@@ -33,4 +42,4 @@ P1: BEGIN
 	-- Cursor left open for client application
 	
 
-END P1
+END P1 
