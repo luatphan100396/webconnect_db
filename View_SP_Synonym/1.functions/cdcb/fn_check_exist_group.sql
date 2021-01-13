637a7f3ffbabb788a3d48a3e2ae17d4c207e6631
linh <pthuylinh@tma.com.vn>
2021-01-13 14:04:09 +0700

    WC-707 add function check exist group

---
+++ View_SP_Synonym/1.functions/cdcb/fn_check_exist_group.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION fn_Check_Exist_Group
+--======================================================
+--Author: Linh Pham
+--Created Date: 2021-01-13
+--Description: Check whether the input Role has been existed
+--======================================================
+(
+	@GROUP_SHORT_NAME VARCHAR(20),
+	@GROUP_KEY INT 
+) 
+RETURNS INTEGER
+
+LANGUAGE SQL
+BEGIN 
+	DECLARE IS_EXISTED SMALLINT DEFAULT 0; 
+
+	SET IS_EXISTED = (SELECT case when COUNT(1)>=1 then 1 else 0 end
+					  FROM GROUP_TABLE uac 
+				      WHERE LOWER(uac.GROUP_SHORT_NAME) = LOWER(@GROUP_SHORT_NAME) 
+				             and GROUP_KEY NOT IN @GROUP_KEY
+					  );
+
+	RETURN IS_EXISTED;
+END
