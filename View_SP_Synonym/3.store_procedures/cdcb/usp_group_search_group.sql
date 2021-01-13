a6b832e5e0ac1b2a04b2d992cd3db2de33370e07
linh <pthuylinh@tma.com.vn>
2021-01-13 13:51:36 +0700

    WC-703 add group_search_group

--- View_SP_Synonym/3.store_procedures/cdcb/usp_group_search_group.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_group_search_group.sql
@@ -8,8 +8,10 @@ CREATE OR REPLACE PROCEDURE usp_Group_Search_Group
 --=================================================================================
 (
 	IN @GROUP_NAME VARCHAR(100)
+	,IN @page_number int
+	,IN @row_per_page int
 )
-	DYNAMIC RESULT SETS 1
+	DYNAMIC RESULT SETS 2
 P1: BEGIN
 	BEGIN
 	-- Declare cursor
@@ -26,13 +28,40 @@ P1: BEGIN
 		LEFT JOIN REFERENCE_TABLE ref
 			on gr.STATUS_CODE = ref.CODE
 			and ref.TYPE ='STATUS_CODE'  
-		INNER JOIN  USER_GROUP_TABLE ugr
+		INNER JOIN  
+		(
+			select distinct GROUP_KEY
+			from USER_GROUP_TABLE
+		) ugr
 			ON ugr.GROUP_KEY=gr.GROUP_KEY
 			WHERE LOWER(GROUP_NAME) LIKE '%'||LOWER(@GROUP_NAME)||'%'
 		ORDER BY GROUP_NAME
+		LIMIT @row_per_page
+		OFFSET (@page_number-1)*@row_per_page
 		WITH UR;
 
 	-- Cursor left open for client application
 	OPEN cursor1;
+	END;
+	
+	BEGIN
+	-- Declare cursor
+	DECLARE cursor2 CURSOR WITH RETURN for
+	SELECT 
+		 	count(1) AS Num_Recs
+		FROM GROUP_TABLE gr
+		LEFT JOIN REFERENCE_TABLE ref
+			on gr.STATUS_CODE = ref.CODE
+			and ref.TYPE ='STATUS_CODE'  
+		INNER JOIN  
+		(
+			select distinct GROUP_KEY
+			from USER_GROUP_TABLE
+		) ugr
+			ON ugr.GROUP_KEY=gr.GROUP_KEY
+			WHERE LOWER(GROUP_NAME) LIKE '%'||LOWER(@GROUP_NAME)||'%'
+		WITH UR;
+	-- Cursor left open for client application
+	OPEN cursor2;
 	END;
 END P1
