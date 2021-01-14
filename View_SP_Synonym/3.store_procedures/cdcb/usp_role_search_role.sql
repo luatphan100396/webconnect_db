ce705d79ce5fc8d48cbf858ef7da3c2aa0b4b4e3
linh <pthuylinh@tma.com.vn>
2021-01-14 11:55:56 +0700

    WC-727 fix bug search role

--- View_SP_Synonym/3.store_procedures/cdcb/usp_role_search_role.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_role_search_role.sql
@@ -7,13 +7,13 @@ CREATE OR REPLACE PROCEDURE usp_Role_Search_Role
 --       +Ds1: table with list role
 --=================================================================================
 (
-  IN @ROLE_NAME VARCHAR(100),
-  IN @page_number INT,
-  IN @row_per_page INT
+  	IN @ROLE_NAME VARCHAR(100)
+  	,IN @page_number int
+	,IN @row_per_page int
 )
 
     
-	DYNAMIC RESULT SETS 1
+	DYNAMIC RESULT SETS 3
 P1: BEGIN
     
 	
@@ -21,8 +21,9 @@ P1: BEGIN
 		DECLARE cursor1 CURSOR WITH RETURN for
 		SELECT 
 		        row_number()over(order by ROLE_NAME) as No,
-		        ROLE_SHORT_NAME,
-				ROLE_NAME,
+				r.ROLE_KEY,
+		        r.ROLE_SHORT_NAME,
+				r.ROLE_NAME,
 				ref.DESCRIPTION as STATUS,
 				case when ur.ROLE_KEY IS NOT NULL then '0' 
 				     else '1'
@@ -44,6 +45,23 @@ P1: BEGIN
 		OPEN cursor1;
 	END;
 	-- Cursor left open for client application
+	BEGIN
+		DECLARE cursor2 CURSOR WITH RETURN for
+		SELECT 
+		    count(1) AS Num_Recs
+		FROM ROLE_TABLE r
+		LEFT JOIN REFERENCE_TABLE ref
+			on r.STATUS_CODE = ref.CODE
+			and ref.TYPE ='STATUS_CODE'
+		LEFT JOIN (
+		     select distinct ROLE_KEY
+		     from USER_ROLE_TABLE  
+		)ur
+		    on ur.ROLE_KEY = r.ROLE_KEY
+		WHERE LOWER(ROLE_NAME) LIKE '%'||LOWER(@ROLE_NAME)||'%'
+		WITH UR;
+		OPEN cursor2;
+	END;
 	
 
 END P1 
