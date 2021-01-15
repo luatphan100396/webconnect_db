e2f451254c9a331b5d7e6b3fe24ba50e14f49eb8
thnghi <thnghi@tma.com.vn>
2021-01-15 15:45:05 +0700

    WC-3293 : process operation

--- View_SP_Synonym/3.store_procedures/cdcb/usp_dataexchange_process_operation.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_dataexchange_process_operation.sql
@@ -17,36 +17,92 @@ P1: BEGIN
     DECLARE query VARCHAR(3000);
     
     DECLARE v_INPUT_PATH VARCHAR(3000);
+    
+    DECLARE SQLCODE INTEGER DEFAULT 0; 
+    DECLARE retcode_Operation INTEGER DEFAULT 0;
+    DECLARE err_message varchar(300);
+    
+
+    
+    -- VALIDATE INPUT
+    IF (select count(1) from DATA_EXCHANGE_OPERATION_TABLE where OPERATION_KEY = @OPERATION_KEY) =0 THEN
+		    SIGNAL SQLSTATE '65000' SET MESSAGE_TEXT = 'The operation key does not exist';
+	END IF;
+     
+    IF (select count(1) from DATA_EXCHANGE_OPERATION_TABLE where OPERATION_KEY = @OPERATION_KEY AND UPPER(STATUS) <>'NEW') >0 THEN
+		    SIGNAL SQLSTATE '65000' SET MESSAGE_TEXT = 'The operation is inprocess or processed';
+	END IF;
+     
      
 	SET v_REQUEST_KEY = (SELECT REQUEST_KEY FROM DATA_EXCHANGE_OPERATION_TABLE WHERE OPERATION_KEY = @OPERATION_KEY); 
-	
 	 
-	select CALL_COMMAND
-	into query
-	from
-	(   select OPERATION_NAME as QUERY_NAME
-		from DATA_EXCHANGE_OPERATION_TABLE d 
-		where OPERATION_KEY = @OPERATION_KEY
-	)d
-	inner join QUERY_TABLE q
-	on d.QUERY_NAME = q.QUERY_NAME
-	;
-	
-	
-	
-    SET query = replace(query,'@REQUEST_KEY',cast(v_REQUEST_KEY as varchar(20)));
-    SET query = replace(query,'@OPERATION_KEY',cast(@OPERATION_KEY as varchar(20)));
-  
-    EXECUTE IMMEDIATE query;
-   
-   
-   
-begin
-     declare c1 cursor with return for
-       select 1 AS RESULT FROM sysibm.sysdummy1;
-     
-     open c1;
-end;
+	 BEGIN  
+ 
+ 
+		     DECLARE EXIT HANDLER FOR SQLEXCEPTION, SQLWARNING, NOT FOUND
+		     SET retcode_Operation = SQLCODE;
+		     
+		      
+			 --START PROCESS
+			 UPDATE DATA_EXCHANGE_OPERATION_TABLE SET STATUS = 'Processing', START_TIME = current timestamp
+			 WHERE OPERATION_KEY = @OPERATION_KEY;
+				
+				
+			 
+			select CALL_COMMAND
+			into query
+			from
+			(   select OPERATION_NAME as QUERY_NAME
+				from DATA_EXCHANGE_OPERATION_TABLE d 
+				where OPERATION_KEY = @OPERATION_KEY
+			)d
+			inner join QUERY_TABLE q
+			on d.QUERY_NAME = q.QUERY_NAME
+			;
+			
+			
+			
+		    SET query = replace(query,'@REQUEST_KEY',cast(v_REQUEST_KEY as varchar(20))); 
+		    SET query = replace(query,'@OPERATION_KEY',cast(@OPERATION_KEY as varchar(20))); 
+		    EXECUTE IMMEDIATE query;
+		    
+		    
+		    UPDATE DATA_EXCHANGE_OPERATION_TABLE SET STATUS = 'Completed', END_TIME = current timestamp
+			WHERE OPERATION_KEY = @OPERATION_KEY;
+			
+			--COMMIT;
+    
+    END;
+			 
+	  	     IF retcode_Operation < 0 THEN 
+				 
+				 SET err_message = (SELECT SYSPROC.SQLERRM (cast(retcode_Operation as varchar(20))) FROM SYSIBM.SYSDUMMY1); 
+				 
+				 UPDATE DATA_EXCHANGE_OPERATION_TABLE  SET   STATUS = 'Failed', MESSAGE = err_message, END_TIME = current timestamp
+			     WHERE OPERATION_KEY = @OPERATION_KEY; 
+				  
+				 
+				 BEGIN
+						DECLARE cursor1 CURSOR WITH RETURN for
+						SELECT retcode_Operation
+						FROM sysibm.sysdummy1;
+					 
+						OPEN cursor1;
+					END;
+				 
+			 ELSE 
+				COMMIT ; 
+				 
+					BEGIN
+						DECLARE cursor1 CURSOR WITH RETURN for
+						SELECT  1 AS RESULT 
+						FROM sysibm.sysdummy1;
+					 
+						OPEN cursor1;
+					END;
+				 
+				 
+			 END IF ;  
  	  		 			
   
 END P1
