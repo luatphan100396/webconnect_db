2c9ede75a57ae8fc8b091474fe501ace6ec110dc
thnghi <thnghi@tma.com.vn>
2021-01-15 10:52:21 +0700

    fix bug login email

--- View_SP_Synonym/3.store_procedures/cdcb/usp_login_get_user_password.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_login_get_user_password.sql
@@ -7,48 +7,31 @@ CREATE OR REPLACE PROCEDURE usp_Login_Get_User_Password
 --        +Ds1: Table with password
 --======================================================
 (
-	IN @USER_NAME VARCHAR(128),
-	IN @EMAIL_ADDRESS VARCHAR(200)
+	IN @USER_NAME_OR_EMAIL_ADDRESS VARCHAR(200) 
 )
 	DYNAMIC RESULT SETS 2
 BEGIN
     
     DECLARE v_USER_KEY int;
-
-    SET @USER_NAME = nullif(trim(@USER_NAME),'');
-    SET @EMAIL_ADDRESS = nullif(trim(@EMAIL_ADDRESS),'');
-    
-    
-    
-	IF  @USER_NAME IS NULL
-	   AND @EMAIL_ADDRESS IS NULL
-		THEN
-		 
-	 	 SIGNAL SQLSTATE '65000' SET MESSAGE_TEXT = 'Input is not valid';
+ 
+	-- Check user name
+	
+	SELECT USER_KEY 
+		INTO v_USER_KEY
+	from USER_ACCOUNT_TABLE 
+	WHERE lower(USER_NAME) = lower(@USER_NAME_OR_EMAIL_ADDRESS) 
+		limit 1;
 		
-	END IF;
+	-- Check email
 	
-	
-	IF @USER_NAME IS NOT NULL THEN
-	
-		SELECT USER_KEY 
-		INTO v_USER_KEY
-		from USER_ACCOUNT_TABLE 
-		WHERE lower(USER_NAME) = lower(@USER_NAME) 
-		limit 1;
-	 
-	 END IF;
-	 
-	 
-	 IF @EMAIL_ADDRESS IS NOT NULL THEN
-	
-		select USER_KEY 
-		INTO v_USER_KEY
+	IF v_USER_KEY IS NULL THEN
+		
+		 SELECT USER_KEY 
+			INTO v_USER_KEY
 		from USER_INFO_TABLE 
-		WHERE lower(EMAIL_ADDR) = lower(@EMAIL_ADDRESS) 
-		limit 1;
-	 
-	 END IF;
+		WHERE lower(EMAIL_ADDR) = lower(@USER_NAME_OR_EMAIL_ADDRESS) 
+			limit 1; 
+    END IF; 
 	 
 	 
 	BEGIN
@@ -62,5 +45,5 @@ BEGIN
 		WITH UR;
 	
 		OPEN cursor1;
-	END;
+	END; 
 END 
