2dd8040508b66d3207f8358d13e678773738612d
thnghi <thnghi@tma.com.vn>
2021-01-18 17:30:33 +0700

    fix missing code

--- View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_fields.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_common_fields.sql
@@ -1,26 +1,24 @@
-CREATE OR REPLACE PROCEDURE usp_Get_Common_Fields_By_Component_Key
+CREATE OR REPLACE PROCEDURE usp_Get_Common_Fields
 --====================================================================================
 --Author: Nghi Ta
 --Created Date: 2021-01-14
---Description: Get List Fields by Component Key 
+--Description: Get List all Fields
 --Output: 
 --       +Ds1: table with fields
 --=======================================================================================
-(
-    
-	IN @COMPONENT_KEY INT
+( 
 )
 	DYNAMIC RESULT SETS 1
 P1: BEGIN
 	-- Declare cursor
 	DECLARE cursor1 CURSOR WITH RETURN for
 	SELECT 
+		 COMPONENT_KEY,
 		 FIELD_KEY, 
 		 FIELD_NAME
-	FROM FIELD_TABLE 
-	WHERE  COMPONENT_KEY = @COMPONENT_KEY 	
-	order by FIELD_NAME
-		WITH UR; 
+	FROM FIELD_TABLE  
+	order by COMPONENT_KEY,FIELD_NAME
+	WITH UR; 
 
 	-- Cursor left open for client application
 	OPEN cursor1;
