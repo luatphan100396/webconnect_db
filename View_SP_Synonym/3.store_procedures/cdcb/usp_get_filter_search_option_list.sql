5175a7838025c143f1e5cccde52dfd4328e20906
linh <pthuylinh@tma.com.vn>
2021-01-25 13:01:51 +0700

    WC-817 fix search option

--- View_SP_Synonym/3.store_procedures/cdcb/usp_get_filter_search_option_list.sql
+++ View_SP_Synonym/3.store_procedures/cdcb/usp_get_filter_search_option_list.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE PROCEDURE usp_Get_Filter_Search_Option_List
 --======================================================
---Author: Nghi Ta
---Created Date: 2020-05-12
+--Author: Linh Pham
+--Created Date: 2020-01-25
 --Description: Define search option list 
 --Output:
 --        +Ds1: Table with OPTION_KEY, OPTION_DESC 
@@ -16,31 +16,52 @@ CREATE OR REPLACE PROCEDURE usp_Get_Filter_Search_Option_List
 --                     SAMPLE_ID_SEX    	Sample ID + Sex Code (20 bytes max)
 --                     SHORT_NAME       	Short Name
 --======================================================
-()
+(
+	IN @SEARCH_FOR VARCHAR(10) -- GOAT/CATTLE
+)
   
   dynamic result sets 1
 BEGIN
     
-         
-	 -- Sex list   
+      IF @SEARCH_FOR ='CATTLE' THEN   
+	 -- Sex list  cattle 
 	BEGIN
 		DECLARE cursor1  CURSOR WITH RETURN for
 	    SELECT  OPTION_KEY, OPTION_DESC
 	    FROM (
-	    VALUES ('ANIM_KEY_17','Animal ID (17 bytes)'),
-			   ('ANIM_KEY_17_SEX','Animal ID + Sex Code (18 bytes)'),
-			   ('SAMPLE_ID_SEX','Sample ID + Sex Code (20 bytes max)'),
-			   ('NAAB_CODE','NAAB Code'),
-			   ('ITB_ID','Interbull ID (19 bytes)'),
-			   ('HERD_ID','Herd ID'),
-			   ('SHORT_NAME','Short Name'),
-			   ('PARTIAL_FULL_NAME','Partial Full Name'),
-			   ('HERD_CTRL_NUM','Herd + Cow Control Number')
+	    VALUES 	('ANIM_KEY_12','Animal ID (12 bytes)'),
+	    		('ANIM_KEY_17','Animal ID (17 bytes)'),
+			   	('ANIM_KEY_17_SEX','Animal ID + Sex Code (18 bytes)'),
+			   	('ITB_ID','Animal Interbull ID (19 bytes)'),
+			    ('SAMPLE_ID_SEX','Sample ID + Sex Code (20 bytes max)'),
+			    ('NAAB_CODE','NAAB Code'),
+			    ('SHORT_NAME','Short Name'),
+			    ('PARTIAL_FULL_NAME','Partial Full Name'),
+			    ('HERD_CTRL_NUM','Herd + Cow Control Number'),
+			    ('HERD_ID','Herd ID')
 	    )t (OPTION_KEY, OPTION_DESC);
 	    
 		OPEN cursor1;
 		  
 	END;
+	ELSEIF @SEARCH_FOR ='GOAT' THEN
+	 -- Sex list  cattle 
+	BEGIN
+		DECLARE cursor2  CURSOR WITH RETURN for
+	    SELECT  OPTION_KEY, OPTION_DESC
+	    FROM (
+	    VALUES  ('ANIM_KEY_6','Last 6 characters.'),
+	    		('ANIM_KEY_17','Animal ID (17 bytes)'),
+	    		('ANIM_KEY_17_SEX','Animal ID + Sex Code (18 bytes)'),
+			    ('SHORT_NAME','Short Name'),
+			    ('PARTIAL_FULL_NAME','Partial Full Name'),
+			    ('HERD_ID','Herd ID')
+	    )t (OPTION_KEY, OPTION_DESC);
+	    
+		OPEN cursor2;
+		  
+	END;
+	END IF;
 	  
 END
 
