d071f7f774fa00ca1799cbd9a82be4cd75a1627b
thnghi <thnghi@tma.com.vn>
2021-01-26 17:28:27 +0700

    clean up script

--- DB_Upgrade/cdcb/20210118.create_input_file_template_table.sql
+++ DB_Upgrade/cdcb/20210118.create_input_file_template_table.sql
@@ -7,24 +7,3 @@ CREATE_DATE DATE not null,
  constraint INPUT_FILE_TEMPLATE_TABLE_PK primary key (NAME)
 );
 
-INSERT INTO INPUT_FILE_TEMPLATE_TABLE
-(
-NAME,
-TEMPLATE_DETAIL,
-CREATE_DATE
-)
-VALUES 
-('INPUT_TEMPLATE_ID_LIST_ANIM_FORMATTED_PEDIGREE',
-'{
- "id_list": ["JEUSA000118662185","JEUSA000067106977","JEUSA000116279413"]
- "request_data": "Get Animal Formatted Pedigree Information"
-}',
-current timestamp
-),
-('INPUT_TEMPLATE_FILE_ANIM_FORMATTED_PEDIGREE',
-'{
- "input_file": "/home/cdcb/upload_folder/TEST_2000_ANIMALS.txt"
- "request_data": "Get Animal Formatted Pedigree Information"
-}',
-current timestamp
-);
